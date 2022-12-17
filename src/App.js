9fd12f46670c42958bfe387e5c75fabc19c69749
unknown <el-bana>
2022-12-17 18:37:22 +0200

    some more cleaning

--- src/App.js
+++ src/App.js
@@ -6,10 +6,21 @@ import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Login from './pages/Forms/Login/Login';
 import Register from './pages/Forms/Register/Register';
+import { useLocation } from 'react-router-dom';
+import clsx from 'clsx'
 
 function App() {
+  const location = useLocation();
+  const isHolyGrid = () => {
+    const paths = ['/login', '/register', '/']
+    console.log(location.pathname)
+    return paths.includes(location.pathname)
+  }
+
   return (
-    <div className="App">
+    <div className={clsx('App', {
+      'holyGrid': isHolyGrid()
+    })}>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
