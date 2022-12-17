4beac0fc087e4d5bb2a723da3b6586c971be260e
unknown <el-bana>
2022-12-17 19:23:02 +0200

    styling man

--- src/pages/Classes/Classes.jsx
+++ src/pages/Classes/Classes.jsx
@@ -3,6 +3,7 @@ import GridItem from '../../components/grid/GridItem'
 import styles from './Classes.module.css'
 import bluePlus from '../../assets/icons/plus-blue.svg'
 import ClassCard from '../../components/classCard/ClassCard'
+import clsx from 'clsx'
 
 const Classes = () => {
   return (
@@ -12,19 +13,19 @@ const Classes = () => {
         <img src={bluePlus} alt='add class' />
       </div>
       <GridContainer className={styles.card_grid}>
-        <GridItem gridSize='4' className={styles.classes_card_container}>
+        <GridItem gridSize='4' className={clsx(styles.classes_card_container, 'halfSizeGridTablet fullSizeGridMobile')}>
           <ClassCard name='FCDS - level 3 simu' />
         </GridItem>
-        <GridItem gridSize='4' className={styles.classes_card_container}>
+        <GridItem gridSize='4' className={clsx(styles.classes_card_container, 'halfSizeGridTablet fullSizeGridMobile')}>
           <ClassCard name='FCDS - level 3 simu' />
         </GridItem>
-        <GridItem gridSize='4' className={styles.classes_card_container}>
+        <GridItem gridSize='4' className={clsx(styles.classes_card_container, 'halfSizeGridTablet fullSizeGridMobile')}>
           <ClassCard name='FCDS - level 3 simu' />
         </GridItem>
-        <GridItem gridSize='4' className={styles.classes_card_container}>
+        <GridItem gridSize='4' className={clsx(styles.classes_card_container, 'halfSizeGridTablet fullSizeGridMobile')}>
           <ClassCard name='FCDS - level 3 simu' />
         </GridItem>
-        <GridItem gridSize='4' className={styles.classes_card_container}>
+        <GridItem gridSize='4' className={clsx(styles.classes_card_container, 'halfSizeGridTablet fullSizeGridMobile')}>
           <ClassCard name='FCDS - level 3 simu' />
         </GridItem>
       </GridContainer>
